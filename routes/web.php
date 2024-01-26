43dd2595e3b2838f43b965483d83182dcf7c2c29
MONOBLOCKER <83527368+monoblocker@users.noreply.github.com>
2024-01-26 09:47:48 +0500

    Научился делать роутинг с запросом get

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Меня зовут Михаил, я backend программист на PHP.";
+});
+
+Route::get('/hobby', function () {
+    return "Мне нравится смотреть играть в игры и слушать музыку разных жанров";
+});
+
+Route::get("/job", function () {
+    return "Пока что я не работаю. Я учусь в университете";
+});
+
+Route::get("/error", function () {
+   return "Похоже, что-то пошло не так";
+});
+
+Route::get("/games", function () {
+    return "Это страница с моими любимыми играми";
+});
+
+Route::get("/movies", function () {
+   return "На этой странице расположены мои любимые фильмы и аниме";
+});
+
+Route::get("/tech", function () {
+    return "На этой странице расположены все технологии, которые я изучил";
+});
+
+Route::get("/social", function () {
+    return "На этой странице ссылки на меня в интернете";
+});
